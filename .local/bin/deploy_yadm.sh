9419b9c54a266a99381c6ba8d98ce5197aa50df6
Anatoly Tsyganenko <github@h-tol.net>
2020-09-02 14:46:20 +0300

    add deploy script

---
+++ .local/bin/deploy_yadm.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+cd
+git clone https://github.com/TheLocehiliosan/yadm.git ~/.local/yadm-project
+mkdir -p ~/.local/bin
+ln -s ~/.local/yadm-project/yadm ~/.local/bin/yadm
+~/.local/bin/yadm clone --no-bootstrap https://github.com/htol/dotfiles.git
