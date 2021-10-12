9443f031b378b29702c364826180a31d60753ac8
htol <github@h-tol.net>
2021-10-12 11:17:16 +0000

    updating plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -4,6 +4,13 @@
 -- Only if your version of Neovim doesn't have https://github.com/neovim/neovim/pull/12632 merged
 --vim._update_package_paths()
 
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
 
 return require('packer').startup(function()
   -- Packer can manage itself
