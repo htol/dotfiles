1b15a6cf15cf440a11d02ec7b4cf1b74f534260c
htol <github@h-tol.net>
2021-10-13 10:31:19 +0000

    trying starify

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -16,4 +16,19 @@ return require('packer').startup(function()
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+
+
+    -- Little know features:
+    --   :SSave
+    --   :SLoad
+    --       These are wrappers for mksession that work great. I never have to use
+    --       mksession anymore or worry about where things are saved / loaded from.
+    use {
+      "mhinz/vim-startify",
+      cmd = { "SLoad", "SSave" },
+      config = function()
+        vim.g.startify_disable_at_vimenter = true
+      end,
+    }
+
 end)
