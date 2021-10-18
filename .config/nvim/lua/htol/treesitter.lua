ae3d0cbea8d78624b1e3e788458700bec00cf1ed
htol <github@h-tol.net>
2021-10-18 10:35:26 +0000

    adding java and kotlin to treesitter

--- .config/nvim/lua/htol/treesitter.lua
+++ .config/nvim/lua/htol/treesitter.lua
@@ -1,4 +1,7 @@
 require'nvim-treesitter.configs'.setup {
-    ensure_installed = {"go", "python", "c", "cpp", "bash", "html", "javascript", "json", "yaml", "lua", "vue"},
+    ensure_installed = {
+        "go", "python", "c", "cpp", "bash", "html", "javascript",
+        "json", "yaml", "lua", "vue", "java", "kotlin"
+    },
     highlight = { enable = true },
 }
