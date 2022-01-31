4858e42a008b40acc38bce305eff2ef2343548a2
htol <github@h-tol.net>
2022-01-31 23:28:29 +0300

    fix lsp diags with 0.6.1

--- .config/nvim/lua/htol/lsp.lua
+++ .config/nvim/lua/htol/lsp.lua
@@ -16,51 +16,17 @@ for type, icon in pairs(signs) do
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] =
-  function(_, _, params, client_id, _)
-    local config = { -- your config
-      underline = false,
-      virtual_text = {
-        prefix = '~',
-        spacing = 2,
-      },
-      signs = true,
-      update_in_insert = true,
-    }
-    local uri = params.uri
-    local bufnr = vim.uri_to_bufnr(uri)
-
-    if not bufnr then
-      return
-    end
-
-    local diagnostics = params.diagnostics
-
-    for i, v in ipairs(diagnostics) do
-      diagnostics[i].message = string.format("%s: %s", v.source, v.message)
-    end
-
-    vim.lsp.diagnostic.save(diagnostics, bufnr, client_id)
-
-    if not vim.api.nvim_buf_is_loaded(bufnr) then
-      return
-    end
-
-    vim.lsp.diagnostic.display(diagnostics, bufnr, client_id, config)
-  end
-
-
---vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
---  vim.lsp.diagnostic.on_publish_diagnostics, {
---    underline = false,
---    virtual_text = {
---      spacing = 2,
---      prefix = '~',
---    },
---    signs = true,
---    update_in_insert = true,
---  }
---)
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = false,
+    virtual_text = {
+      spacing = 2,
+      prefix = '~',
+    },
+    signs = true,
+    update_in_insert = true,
+  }
+)
 
 lsp.tsserver.setup{ on_attach=on_attach }
 
