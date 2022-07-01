2d6e048b8a72311e21eac4e03496395d124a696a
htol <github@h-tol.net>
2022-07-01 13:45:28 +0200

    make l and ll works on mac without warnings

--- .config/fish/functions/l.fish
+++ .config/fish/functions/l.fish
@@ -1,3 +1,7 @@
 function l --wraps=ls --description 'List contents of directory using long format'
-    ls -lvh --group-directories-first $argv
+  if test "$OSTYPE" = "linux-gnu"  # Is this the Ubuntu system?
+      ls -lhv --group-directories-first $argv
+  else
+      ls -lhv $argv
+  end
 end
--- .config/fish/functions/ll.fish
+++ .config/fish/functions/ll.fish
@@ -1,3 +1,7 @@
 function ll --wraps=ls --description 'List contents of directory using long format for all files'
-    ls -alvh --group-directories-first $argv
+  if test "$OSTYPE" = "linux-gnu"  # Is this the Ubuntu system?
+      ls -alhv --group-directories-first $argv
+  else
+      ls -alhv $argv
+  end
 end
