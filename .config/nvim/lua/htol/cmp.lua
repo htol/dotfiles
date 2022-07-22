2092335b1d05d51ad5b4598c05509c8e7c0ac5d5
htol <github@h-tol.net>
2022-07-22 17:48:36 +0200

    nit: cmp tuning

--- .config/nvim/lua/htol/cmp.lua
+++ .config/nvim/lua/htol/cmp.lua
@@ -8,7 +8,7 @@ cmp.setup({
   mapping = {
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-y>'] = cmp.config.disable,
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
@@ -25,12 +25,12 @@ cmp.setup({
       format = function(entry, vim_item)
         -- fancy icons and a name of kind
         vim_item.kind = require("lspkind").presets.default[vim_item.kind] .. " " .. vim_item.kind
-    
+
         -- set a name for each source
         vim_item.menu = ({
           buffer = "[Buffer]",
           nvim_lsp = "[LSP]",
-          luasnip = "[LuaSnip]",
+          luasnip = "[Snippet]",
           nvim_lua = "[Lua]",
           latex_symbols = "[Latex]",
         })[entry.source.name]
