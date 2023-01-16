9e5bff5da6a74b0f8e44279bdd9a7ea26eae3096
htol <github@h-tol.net>
2023-01-16 18:53:01 +0100

    add sudo wrapper

---
+++ .config/fish/functions/psudo.fish
@@ -0,0 +1,9 @@
+function psudo
+  set is_admin (/usr/sbin/dseditgroup -q -o checkmember -m "$(/usr/bin/stat -f%Su /dev/console)" admin)
+  if string match -q -r '^no.+$' $is_admin; then
+    /Applications/Privileges.app/Contents/Resources/PrivilegesCLI --add &&
+    /usr/bin/sudo $argv
+  else
+    /usr/bin/sudo $argv
+  end
+end
