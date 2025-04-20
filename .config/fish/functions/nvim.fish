31ec8bec388670e2c3e4f8e8d551e61acdbbd8a0
htol <github@h-tol.net>
2025-04-20 15:13:53 +0200

    updating neovim to v0.11

--- .config/fish/functions/nvim.fish
+++ .config/fish/functions/nvim.fish
@@ -1,6 +1,6 @@
 function nvim
   if test (uname -s) = "Linux"
-      ~/.local/nvim/nvim-linux64/bin/nvim $argv
+      ~/.local/nvim/nvim-linux-x86_64/bin/nvim $argv
   else
       /usr/bin/env nvim $argv
   end
