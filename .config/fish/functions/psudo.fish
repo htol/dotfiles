d82d78be4bb1db4f31d79f500eaa0fbe20feb14a
Anatoly Tsyganenko <anatoly.tsyganenko@booking.com>
2025-04-21 23:07:33 +0200

    fish function fix

--- .config/fish/functions/psudo.fish
+++ .config/fish/functions/psudo.fish
@@ -1,6 +1,6 @@
 function psudo
   set is_admin (/usr/sbin/dseditgroup -q -o checkmember -m "$(/usr/bin/stat -f%Su /dev/console)" admin)
-  if string match -q -r '^no.+$' $is_admin; then
+  if string match -q -r '^no.+$' $is_admin
     /Applications/Privileges.app/Contents/Resources/PrivilegesCLI --add &&
     /usr/bin/sudo $argv
   else
