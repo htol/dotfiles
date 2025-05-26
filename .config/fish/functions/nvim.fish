72f5e48234a0478eeb08357cb71ce1a819270428
htol <github@h-tol.net>
2025-05-26 19:01:37 +0200

    fix: XDG_DATA_HOME for nvim prefix

--- .config/fish/functions/nvim.fish
+++ .config/fish/functions/nvim.fish
@@ -1,6 +1,7 @@
 function nvim
   if test (uname -s) = "Linux"
-      ~/.local/nvim/nvim-linux-x86_64/bin/nvim $argv
+      set PREFIX (if set -q XDG_DATA_HOME; echo $XDG_DATA_HOME; else '~/.local';end)
+      $PREFIX/nvim/nvim-linux-x86_64/bin/nvim $argv
   else
       /usr/bin/env nvim $argv
   end
