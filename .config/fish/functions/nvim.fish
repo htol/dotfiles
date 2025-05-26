557fcd2a32eec8e6adb98b2e3b72d2016536003f
htol <github@h-tol.net>
2025-05-26 21:46:01 +0200

    fix nvim path

--- .config/fish/functions/nvim.fish
+++ .config/fish/functions/nvim.fish
@@ -1,6 +1,6 @@
 function nvim
   if test (uname -s) = "Linux"
-      set PREFIX (if set -q XDG_DATA_HOME; echo $XDG_DATA_HOME; else; echo '~/.local';end)
+      set PREFIX (if set -q XDG_DATA_HOME; echo $XDG_DATA_HOME; else; echo $HOME'/.local';end)
       $PREFIX/nvim/nvim-linux-x86_64/bin/nvim $argv
   else
       /usr/bin/env nvim $argv
