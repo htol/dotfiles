152ffc7513412b84d80d1c0475cf425a8c367a24
htol <github@h-tol.net>
2025-06-12 19:41:35 +0200

    lualine refactoring

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,6 +1,37 @@
 return {
-    {
-        'nvim-lualine/lualine.nvim', -- Fancier statusline
-        dependencies = { 'nvim-tree/nvim-web-devicons' }
-    },
+  {
+    'nvim-lualine/lualine.nvim', -- Fancier statusline
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      local function short_hostname()
+        local fqdn = vim.uv.os_gethostname()
+        local _, _, host = string.find(fqdn, "^(.-)%..*$")
+        return host == nil and fqdn or host
+      end
+
+      require('lualine').setup {
+        options = {
+          icons_enabled = true,
+          --theme = 'gruvbox',
+          theme = 'nord',
+          component_separators = '|',
+          section_separators = '',
+          disabled_filetypes = {},
+        },
+        sections = {
+          lualine_c = {
+            'filename',
+          },
+          lualine_x = {
+            { 'diagnostics', sources = { "nvim_diagnostic" }, symbols = { error = ' ', warn = ' ', info = ' ',
+              hint = ' ' } },
+            'encoding',
+            'filetype'
+          },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location', { short_hostname } }
+        },
+      }
+    end
+  },
 }
