7430a4b7d236b57736b36bbe950c0a1f1832e1d7
htol <github@h-tol.net>
2025-06-12 19:43:27 +0200

    formatting

--- .config/nvim/after/plugin/diagnostic.lua
+++ .config/nvim/after/plugin/diagnostic.lua
@@ -4,22 +4,21 @@ vim.diagnostic.config({
   severity_sort = true,
   update_in_insert = true,
   float = {
-      source = true,
-      header = "",
-      border = "rounded",
+    source = true,
+    header = "",
+    border = "rounded",
   },
-  jump = {float = true, wrap = false},
--- from old config for reference
--- signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
+  jump = { float = true, wrap = false },
   signs = {
-      text = {
-          [vim.diagnostic.severity.ERROR] = "✗",
-          [vim.diagnostic.severity.WARN] = "▲",
-          [vim.diagnostic.severity.INFO] = "∙",
-          [vim.diagnostic.severity.HINT] = "∴"
-      },
-      severity = { min = vim.diagnostic.severity.HINT },
+    text = {
+      [vim.diagnostic.severity.ERROR] = "✗",
+      [vim.diagnostic.severity.WARN] = "▲",
+      [vim.diagnostic.severity.INFO] = "∙",
+      [vim.diagnostic.severity.HINT] = "∴"
+    },
+    severity = { min = vim.diagnostic.severity.HINT },
   },
 })
 
-vim.keymap.set('n', 'td', function() vim.diagnostic.enable(not vim.diagnostic.is_enabled()) end, {desc = 'Toggle diagnostic'})
+vim.keymap.set('n', 'td', function() vim.diagnostic.enable(not vim.diagnostic.is_enabled()) end,
+  { desc = 'Toggle diagnostic' })
